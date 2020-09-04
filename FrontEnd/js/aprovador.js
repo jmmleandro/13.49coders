47c094e107c824e099d04688972d89cdb5bc75fc
Brchap <brchap@gmail.com>
2020-09-04 01:44:06 -0300

    Fix tabela solicitacoes, alteracao cor bordaFiltro, formataData

--- FrontEnd/js/aprovador.js
+++ FrontEnd/js/aprovador.js
@@ -7,7 +7,7 @@ function carregaInfo(){
 
     var linhaHTML = `<div class="row">
                         <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
-                            <img src="${objUser.linkFoto}" width="100%">
+                            <img class="img-fluid img-thumbnail" src="${objUser.linkFoto}">
                         </div>
                         <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
                             <strong>Nome: </strong> ${objUser.nome}<br>
@@ -33,25 +33,47 @@ function onChangeSelectStatus(status){
 function trataConteudoDaLista(lista){
     // aqui efetivamente eu quero trabalhar com os objetos que eu recebi do BackEnd
     var conteudoHTML = "";
-    for (i=0; i<lista.length; i++){
-        var solic = lista[i];
-
-        conteudoHTML = conteudoHTML +`<div class="row">
-                                        <div class="col-1"> ${solic.nomeTecnico} </div>
-                                        <div class="col-4"> ${solic.operadora} <br>
-                                        <div class="col-3"> ${solic.pdv.nome} </div>
-                                        <div class="col-2"> ${solic.dataSolicitacao} ${solic.horaSolicitacao} </div>
-                                        <div class="col-2"> <button type="button" class="btn btn-success" 
-                                                                    onclick="atualizarStatus(${solic.numSeq},1)"> &nbsp; </button>
-                                                            <button type="button" class="btn btn-danger" 
-                                                                    onclick="atualizarStatus(${solic.numSeq},2)"> &nbsp; </button>
-                                                            <button type="button" class="btn btn-secondary" 
-                                                                    onclick="atualizarStatus(${solic.numSeq},3)"> &nbsp; </button>
-                                        </div>
-                                    </div>`;
-
+    if(lista.length == 0){
+        document.getElementById("divTabelaSolic").innerHTML = `<small class="text-muted">No momento, não há solicitações pendentes com esse status</small>`;
     }
-    document.getElementById("listaDeSolic").innerHTML = `${conteudoHTML}`;
+    else
+    {
+        conteudoHTML = `<div class="bordaFiltro">
+                        <legend><b><h6>DADOS DA SOLICITAÇÃO</h6></b></legend><br>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Técnico</th>
+                                    <th scope="col">Operadora</th>
+                                    <th scope="col">PDV</th>
+                                    <th scope="col">Data</th>
+                                    <th scope="col">Aprovar</th>
+                                    <th scope="col">Negar</th>
+                                    <th scope="col">Cancelar</th>
+                                </tr>
+                            </thead>
+                        <tbody id="tbodySolic">`
+                        
+        for (i=0; i<lista.length; i++){
+            var solic = lista[i];
+
+            conteudoHTML = conteudoHTML +`<tr>
+                                            <th scope="row"> ${solic.numSeq} </th>
+                                            <td> ${solic.nomeTecnico} </td>
+                                            <td> ${solic.operadora} </td>
+                                            <td> ${solic.pdv.nome} </td>
+                                            <td> ${formataDateSQL(solic.dataSolicitacao)} ${solic.horaSolicitacao} </td>
+                                            <td> <button type="button" class="btn btn-success" onclick="atualizarStatus(${solic.numSeq},1)"> A </button> </td>
+                                            <td> <button type="button" class="btn btn-danger" onclick="atualizarStatus(${solic.numSeq},2)"> N </button> </td>
+                                            <td> <button type="button" class="btn btn-secondary" onclick="atualizarStatus(${solic.numSeq},3)"> C </button> </td>
+                                        </tr>`;
+
+        }
+        conteudoHTML += `</tbody></table></div>`
+        document.getElementById("divTabelaSolic").innerHTML = `${conteudoHTML}`;
+    }
+    
 
 }
 
@@ -78,8 +100,7 @@ function atualizarStatus(numReq, novoStatus){
 
 function atualizaDash(res){
     if(res.status == 200){
-        onChangeSelectStatus(0);
-        document.getElementById("selectStatus").selectedIndex = 0;
+        onChangeSelectStatus(document.getElementById("selectStatus").selectedIndex);
     }
     else{
         alert("Algo deu errado");
@@ -89,4 +110,10 @@ function atualizaDash(res){
 function logout(){
     localStorage.removeItem("pdvUser");
     window.location = "index.html";
+}
+
+function formataDateSQL(isoFormatDateString){
+    var dateParts = isoFormatDateString.split("-");
+    var jsDate = dateParts[2]+'/'+dateParts[1]+'/'+dateParts[0];
+    return jsDate;
 }
